7e6bebfb15521b636d225563babe24ab0d7f88cc
jihwan <jjhinu1004@gmail.com>
2026-05-08 18:58:05 +0900

    Configure payment RestClient timeouts

---
+++ payment/src/main/java/com/example/paymentsystem/payment/config/CardClientConfig.java
@@ -0,0 +1,43 @@
+package com.example.paymentsystem.payment.config;
+
+import org.apache.hc.client5.http.config.ConnectionConfig;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.core5.util.Timeout;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class CardClientConfig {
+
+    @Bean
+    public RestClient cardRestClient(@Value("${payment.card.base-url}") String cardBaseUrl) {
+        PoolingHttpClientConnectionManager connectionManager =
+                PoolingHttpClientConnectionManagerBuilder.create()
+                        .setMaxConnTotal(50)
+                        .setMaxConnPerRoute(20)
+                        .setDefaultConnectionConfig(ConnectionConfig.custom()
+                                .setConnectTimeout(Timeout.ofMilliseconds(1_000))
+                                .setSocketTimeout(Timeout.ofMilliseconds(3_000))
+                                .build())
+                        .build();
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectionRequestTimeout(Timeout.ofMilliseconds(500))
+                        .build())
+                .build();
+
+        return RestClient.builder()
+                .baseUrl(cardBaseUrl)
+                .requestFactory(new HttpComponentsClientHttpRequestFactory(httpClient))
+                .build();
+    }
+}
